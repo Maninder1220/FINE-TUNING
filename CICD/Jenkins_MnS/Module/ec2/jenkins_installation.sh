48f87c29a11a3e0ba44f1c43f15e863054c724b5
Maninder1220 <maninderjitsinghjaura@gmail.com>
2024-08-08 15:33:25 +0530

    CICD Updation

--- CICD/Jenkins_MnS/Module/ec2/jenkins_installation.sh
+++ CICD/Jenkins_MnS/Module/ec2/jenkins_installation.sh
@@ -2,32 +2,28 @@
 sudo apt update
 
 # Install Java 
-sudo apt install openjdk-21-jdk
+sudo apt install -y openjdk-21-jdk
 
 # Update Package Lists
 sudo apt update
 
 
-# Install Jenkins
+# Download Jenkins Key
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
 
-# Add Repo Key
-sudo wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo gpg --dearmor -o /usr/share/keyrings/jenkins.
-
-
-# Add Repo
-sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list'
-
-# Update Package Lists
 sudo apt update
 
-
 # Install Jenkins
-sudo apt install jenkins
+sudo apt install -y jenkins
 
 # Start n Enable Jenkins Service
-sudo systemctl start jenkins
 sudo systemctl enable jenkins
-sudo systemctl status jenkins
+sudo systemctl start jenkins
+
 
 # Check Java Version
 java -version
